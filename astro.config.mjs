e1278aa2303560d71e63f6bc2697a112bef95bf8
riyooho <66687025+riyooho@users.noreply.github.com>
2026-05-10 14:55:07 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://riyooho.github.io',
+  site: 'https://riyooho.github.io/riyooho.github.io',
   base: '/riyooho.github.io',
   integrations: [
     tailwind(),
